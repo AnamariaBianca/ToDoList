e3918f96243abc8e66264c4183ebc17a8157a787
AnamariaBianca <73616652+AnamariaBianca@users.noreply.github.com>
2020-10-30 20:16:46 +0200

    create route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/todolist',function(){
+	return view('todolist.index');
+});
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
