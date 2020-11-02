7100047891925d960ba29b5fd2cf7ae2f8d0706e
AnamariaBianca <73616652+AnamariaBianca@users.noreply.github.com>
2020-11-02 23:41:48 +0200

    .

--- app/ToDo.php
+++ app/ToDo.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ToDo extends Model
 {
     protected $fillable=['title'];
+
+    // public function getRouteKeyName()
+    // {
+    // 	return 'title';
+    // }
 }
--- resources/views/ToDoList/edit.blade.php
+++ resources/views/ToDoList/edit.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
-	{{$todo->title}}
+	<h1 class="text-2xl">To do list</h1>
+        <x-alert />
+        <form method="post" action="/todolist/create" class="py-5">
+            @csrf
+            <input type="text" name="title" class="py-2 px-2 border rounded"/>
+            <input type="submit" value="Create" class ="p-1 border rounded-lg"/>
+        </form>
+
+        <a href="/todolist" class="m-5 py-1 px-1 bg-white-400 border cursor-pointer rounded text-black"> Back</a>
 
 @endsection
