077935a11b483d37b80f0e19dee32e4bb797897e
AnamariaBianca <73616652+AnamariaBianca@users.noreply.github.com>
2020-11-03 02:56:50 +0200

    not working but ok

--- resources/views/ToDoList/index.blade.php
+++ resources/views/ToDoList/index.blade.php
@@ -10,31 +10,27 @@
 			@foreach($todos as $todo)
 
 			<li class="flex justify-between p-2">
-				<p> {{$todo->title}}</p>
+				<div>
+
+				@include('todolist.complete-button') 
+
+			</div>
+
+				<p> {{$todo->title}}</p> 
 				<div>
 					
 					<a href="{{'/todolist/'.$todo->id.'/edit'}}" class="text-yellow-400 cursor-pointer  text-white"><span class="fas fa-edit px-2"></a>
-						@if($todo->completed)
-						<span class="fas fa-check text-green-500 cursor-pointer px-2" onclick="event.preventDefault(); document.getElementById('form-incomplete-{{$todo->id}}').submit()"/>
-							<form style="display:none" id="{{'form-incomplete-'.$todo->id}}" method="post" action="{{route('todo.incomplete',$todo->id)}}">
-						@csrf
-						@method('delete')
-					</form>
 
 
-						@else
-					
 
-						<span onclick="event.preventDefault(); document.getElementById('form-complete-{{$todo->id}}').submit()" class="fas fa-check text-gray-400 px-2 cursor-pointer px-2" />
-							<form style="display:none" id="{{'form-complete-'.$todo->id}}" method="post" action="{{route('todo.complete',$todo->id)}}">
-						@csrf
-						@method('put')
-					</form>
-							@endif
-					<!-- <span class="fas fa-trash text-red-400 px-2 cursor-pointer" onclick="event.preventDefault(); document.getElementById('form-delete-{{$todo->id}}').submit()"/>
+
+					<span class="fas fa-trash text-red-400 px-2 cursor-pointer" onclick="event.preventDefault(); 
+					if(confirm('Are you sure?')){
+					document.getElementById('form-delete-{{$todo->id}}').submit()
+				}"/>
 					<form style="display:none" id="{{'form-delete-'.$todo->id}}" method="post" action="{{route('todo.delete',$todo->id)}}">
 						@csrf
-						@method('delete') -->
+						@method('delete')
 					</form>
 				</div>
 				
